774aed27cdd7e028267361746eb6fbcacff55748
qandil <qandiloa@gmail.com>
2024-05-21 23:58:45 +0300

    fetcher-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,54 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import "./App.css";
+import "@fontsource/poppins"; // Ensure Poppins font is applied globally in your index.css or Chakra theme
+import NavBar from "./Components/NavBar";
+import MainContent from "./Hooks/MainContent";
+import { ContentContextProvider } from "./Hooks/ContentContext";
+import SidebarAppear from "./SidebarAppear";
+import { OrderStatusProvider } from "./Hooks/OrderStatusContext";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <>
+      <ContentContextProvider>
+        <OrderStatusProvider>
+          <Grid
+            h="100vh" // Ensure the grid takes full viewport height if necessary
+            templateAreas={{
+              base: `"nav" "main"`,
+              md: `"nav nav" "aside main"`,
+            }}
+            templateColumns={{
+              base: "1fr",
+              md: "250px 1fr",
+            }}
+            gap="2" // Optional: Adjust as needed for spacing between grid items
+            backgroundColor="white"
+          >
+            <GridItem area={"nav"}>
+              <NavBar />
+            </GridItem>
+            <GridItem
+              area={"aside"}
+              display={{ base: "none", md: "block" }}
+              p={4} // Optional: Adds padding inside the sidebar
+            >
+              <SidebarAppear />
+            </GridItem>
+            <GridItem
+              area={"main"}
+              p={4} // Adds padding inside the main content area for spacing
+              overflow="auto" // Ensures content within main can scroll if it overflows
+            >
+              <main>
+                <MainContent />
+              </main>
+            </GridItem>
+          </Grid>
+        </OrderStatusProvider>
+      </ContentContextProvider>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-
-Amplify.configure(outputs);
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../src/themes/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
